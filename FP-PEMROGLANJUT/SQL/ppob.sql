fe9c8fdcc2f2152bf56436034ea2938abe3c0aa6
Yy <akumakumu@gmail.com>
2021-12-10 09:48:37 +0700

    query akun admin

--- FP-PEMROGLANJUT/SQL/ppob.sql
+++ FP-PEMROGLANJUT/SQL/ppob.sql
@@ -8,3 +8,8 @@ GO
 
 -- Table akun
 CREATE TABLE akun (usrname varchar(32) NOT NULL PRIMARY KEY, email varchar(64) NOT NULL, passwd varchar(32) NOT NULL);
+
+INSERT INTO akun (usrname, email, passwd)
+VALUES ('admin', 'admin@rocketmail.com', 'notadmin');
+
+SELECT * FROM akun;
