c4aea9a572e9c325c232f9ac8bec5b5cd6c768e7
daffanizwar <daffanizwar101@gmail.com>
2021-12-17 20:07:42 +0700

    Register Controller

---
+++ FP-PEMROGLANJUT/Controller/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace FP_PEMROGLANJUT.Controller
+{
+    class RegisterController
+    {
+        Model.RegisterModel register;
+        View.BuatAkunPage buat;
+
+        public RegisterController(View.BuatAkunPage buat)
+        {
+            register = new Model.RegisterModel();
+            this.buat = buat;
+        }
+
+        public void Buat()
+        {
+            register.namadpn = buat.txtNamaDepan.Text;
+            register.namablkg = buat.txtNamaBelakang.Text;
+            register.email = buat.txtEmail.Text;
+            register.telp = buat.txtNomorTelephon.Text;
+            
+            bool result = buat.InsertRegister();
+            if (result)
+            {
+                MessageBox.Show("Akun berhasil dibuat, " +
+                    "silahkan buat password");
+                View.LoginPage login = new View.LoginPage();
+                login.Show();
+                buat.Close();
+            }
+            else
+            {
+                MessageBox.Show("Maaf, registrasi akun gagal, " +
+                    "periksa dan lengkapi data anda");
+            }
+        }
+            
+    }
+}
--- FP-PEMROGLANJUT/Model/RegisterModel.cs
+++ FP-PEMROGLANJUT/Model/RegisterModel.cs
@@ -12,6 +12,7 @@ namespace FP_PEMROGLANJUT.Model
         public string namadpn { get; set; }
         public string namablkg { get; set; }
         public string email { get; set; }
+        public string telp { get; set; } 
 
 
         public RegisterModel()
@@ -23,7 +24,7 @@ namespace FP_PEMROGLANJUT.Model
         {
             bool result = false;
             DataSet ds = new DataSet();
-            ds = temp.Select("nama depan", "nama belakang = '" + namadpn + namablkg + "' AND email = '" + email + "'");
+            ds = temp.Select("nama depan", "nama belakang", "mail = '" + namablkg + email + "' AND telp = '" + telp + "'");
             if (ds.Tables[0].Rows.Count > 0)
             {
                 result = true;
@@ -33,6 +34,12 @@ namespace FP_PEMROGLANJUT.Model
                 result = false;
             }
             return result;
+        }
+
+        public bool InsertRegister()
+        {
+            string data = "'" + namadpn + "','" + namablkg + "','" + email + "','" + telp + "'";
+            return temp.Insert("register", data);
         }
     }
 }
